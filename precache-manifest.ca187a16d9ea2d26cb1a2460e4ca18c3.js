0f8753f1df93cb8f18f9795ea13c2fd3e7d3b317
karthik <karthik@GCCYL3BBJ1WK.local>
2020-07-18 23:51:19 +0530

    Updates

---
+++ precache-manifest.ca187a16d9ea2d26cb1a2460e4ca18c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459180f8229dde5ee3cdc4b9c21ed0db",
+    "url": "/chennai-covid/index.html"
+  },
+  {
+    "revision": "fb8686674dea7aef139a",
+    "url": "/chennai-covid/static/css/main.2ced2cf4.chunk.css"
+  },
+  {
+    "revision": "17af6804c73ea336ff12",
+    "url": "/chennai-covid/static/js/2.265f403f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chennai-covid/static/js/2.265f403f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8686674dea7aef139a",
+    "url": "/chennai-covid/static/js/main.412f15c1.chunk.js"
+  },
+  {
+    "revision": "2970ac22dc859b005969",
+    "url": "/chennai-covid/static/js/runtime-main.5a77a3b5.js"
+  }
+]);
